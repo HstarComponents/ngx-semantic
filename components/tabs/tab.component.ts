95c796243d20413aabd5a68301a84ff480b01dba
Jay.M.Hu <jay.m.hu@newegg.com>
2016-04-23 17:33:47 +0800

    Update tabset component.

--- components/tabs/tab.component.ts
+++ components/tabs/tab.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, TemplateRef} from "angular2/core";
+import {Component, Input, Host, TemplateRef, OnInit, OnDestroy } from "angular2/core";
+import {Tabset} from './tabset.component';
 
 @Component({
     selector: "s-tab",
@@ -6,23 +7,39 @@ import {Component, Input, TemplateRef} from "angular2/core";
 <div class="ui tab" [attr.data-tab]="tabId" [class.active] = "active">
   <ng-content></ng-content>
 </div>
-
-    `
+`
 })
-export class Tab {
+export class Tab implements OnInit, OnDestroy{
 
     private tabId: string;
+
+    private tabset: Tabset;
 
     @Input()
     public heading: string = '';
 
     @Input()
     public active: boolean = false;
-    
-    @Input() 
+
+    @Input()
     public removable: boolean = false;
 
-    public constructor() {
-        this.tabId = 'tab_' + Date.now() + Math.floor((Math.random() * 10000))
+    public constructor( @Host() tabset: Tabset) {
+        this.tabset = tabset;
+        this.setTabId();
+        this.tabset.addTab(this);
+    }
+
+    private setTabId(): void {
+        var rndStr = `${Date.now()}_${Math.floor((Math.random() * 1000) + 1000)}`;
+        this.tabId = `tab_${rndStr}`;
+    }
+
+    public ngOnInit(): void {
+       this.removable = !!this.removable;
+    }
+
+    public ngOnDestroy(): void {
+        this.tabset.removeTab(this);
     }
 }
--- components/tabs/tabset.component.ts
+++ components/tabs/tabset.component.ts
@@ -1,44 +1,69 @@
-import {ContentChildren, Component, QueryList, Input, AfterContentInit, ElementRef, EventEmitter, Output, Inject} from "angular2/core";
-import {Tab} from "./tab.component";
+import {ContentChildren, Component, QueryList, Input, AfterContentInit, ElementRef, EventEmitter, Output, Injectable} from "angular2/core";
+import {Tab} from './tab.component';
 
 @Component({
   selector: "s-tabset",
   template: `
 <div class="ui tabular menu">
-  <div class="item" [attr.data-tab]="tab.tabId" *ngFor="#tab of tabs" [class.active]="tab.active" (click)="changeActiveTab(tab);">
+  <div class="item" [attr.data-tab]="tab.tabId" *ngFor="#tab of tabs" [class.active]="tab.active"
+   [class.removable]="tab.removable" (click)="changeActiveTab(tab);">
     {{tab.heading}}
-    <a class="float-right" *ngIf="tab.removable">*</a>
+    <i class="remove circle icon" *ngIf="tab.removable" (click)="removeTab(tab)"></i>
   </div>
 </div>
 <ng-content></ng-content>
-`
+`,
+  styles: [
+    '.remove.icon.circle{margin-left: 10px !important; margin-top: -15px !important}',
+    '.item.removable{padding-right: 0 !important;}',
+    'i.remove.circle.icon:hover{color: red; cursor: pointer;}'
+  ]
 })
+@Injectable()
 export class Tabset implements AfterContentInit {
-  
+
   private nativeElement;
 
-  @ContentChildren(Tab)
-  public tabs: QueryList<Tab>;
+  private tabs: Array<Tab> = [];
 
   @Output()
   public onSelect = new EventEmitter(false);
 
-  public constructor( @Inject(ElementRef) templateRef: ElementRef) {
-    this.nativeElement = templateRef.nativeElement;
+  @Output()
+  public onTabRemoved = new EventEmitter(false);
+
+  public constructor() {
   }
 
-  changeActiveTab(tab: Tab) {
-    const tabs = this.tabs.toArray();
-    tabs.forEach(tab => tab.active = false);
+  private setActiveTabAfterActiveTabRemoved(index: number): void {
+    let len = this.tabs.length;
+    this.tabs[(len > index ? index : (len - 1))].active = true;
+  }
+
+  public changeActiveTab(tab: Tab): void {
+    this.tabs.forEach(tab => tab.active = false);
     tab.active = true;
-    this.onSelect.emit(tabs.indexOf(tab));
+    this.onSelect.emit(this.tabs.indexOf(tab));
   }
 
-  ngAfterContentInit() {
+  public addTab(tab: Tab): void {
+    this.tabs.push(tab);
+  }
+
+  public removeTab(tab: Tab): void {
+    var idx = this.tabs.indexOf(tab);
+    this.tabs.splice(idx, 1);
+    this.onTabRemoved.emit(tab);
+    if (tab.active) {
+      tab.active = false;
+      this.setActiveTabAfterActiveTabRemoved(idx);
+    }
+  }
+
+  public ngAfterContentInit(): void {
     //设置默认选中第一个
-    const readTabs = this.tabs.toArray();
-    const activeTab = readTabs.filter(tab => tab.active === true);
-    if (activeTab.length === 0 && readTabs.length > 0)
-      readTabs[0].active = true;
+    const activeTab = this.tabs.filter(tab => tab.active === true);
+    if (activeTab.length === 0 && this.tabs.length > 0)
+      this.tabs[0].active = true;
   }
 }
